abcc98377082869263db634676f0179d9a0a92ee
Laurent Hirth <laurent.hirth.68@icloud.com>
2022-11-26 06:51:39 +0100

    add empty components for home

---
+++ src/app/home/stock-input/stock-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-input',
+  templateUrl: './stock-input.component.html',
+  styleUrls: ['./stock-input.component.scss']
+})
+export class StockInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
