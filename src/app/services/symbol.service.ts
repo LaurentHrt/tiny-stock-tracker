851082c03a19e71fe1de0ddc90b53c7432bd2fcc
Laurent Hirth <laurent.hirth.68@icloud.com>
2022-11-26 09:21:57 +0100

    add symbol & stock services

---
+++ src/app/services/symbol.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SymbolService {
+  private symbolList: string[] = ['TSLA', 'AAPL'] // TODO retrieve localstorage
+
+  constructor() {}
+
+  getAllSymbols(): string[] {
+    console.log('Getting all symbol', this.symbolList)
+    return this.symbolList
+  }
+
+  addSymbol(symbol: string) {
+    // TODO check if symbol exists
+    this.symbolList.push(symbol)
+    // TODO add in localstorage
+    console.log('symbol', symbol, 'added')
+  }
+
+  removeSymbol(symbol: string) {
+    const index = this.symbolList.findIndex((s) => s === symbol)
+    if (index > -1) {
+      this.symbolList.splice(index, 1)
+      console.log('symbol', symbol, 'removed')
+    }
+  }
+}
