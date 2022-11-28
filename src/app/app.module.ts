7eef77071383d598f3b394b5f9f9f0059aa8ea70
Laurent Hirth <laurent.hirth.68@icloud.com>
2022-11-28 13:19:13 +0100

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { SentimentModule } from './sentiment/sentiment.module'
+import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SentimentModule],
+  imports: [BrowserModule, AppRoutingModule, SentimentModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
